7c08e0f2c046d2ea3654f2db990b0bd636492228
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-28 22:56:00 +0700

    adding search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
         <div className="flex justify-between items-center h-20 px-4 absolute top-0 z-10 w-full text-white">
             {/* Desktop Menu */}
             <div>
-                <h1 onClick={handleNav} className={Logo ? 'hidden' : 'block'}>INDONESIA.</h1>
+                <h1 onClick={handleNav} className={Logo ? 'hidden' : 'block'}>ACEH.</h1>
             </div>
             <ul className='hidden md:flex'>
                 <li>Home</li>
@@ -43,7 +43,7 @@ function Navbar() {
             {/* Mobile Menu Dropdown */}
             <div onClick={handleNav} className={Nav ? 'text-black flex flex-col absolute top-0 left-0 w-full bg-gray-100/90 px-4 py-7' : 'absolute left-[-100%]'}>
                 <ul>
-                    <h1>INDONESIA.</h1>
+                    <h1>ACEH.</h1>
                     <li className='border-b'>Home</li>
                     <li className='border-b'>Destinations</li>
                     <li className='border-b'>Travel</li>

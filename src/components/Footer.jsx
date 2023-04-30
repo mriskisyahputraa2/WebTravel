2ec7c238c52c8d5664520b16494d959aafe81e34
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-30 22:09:53 +0700

    finished web-travel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className="w-full bg-gray-100 pt-16">
             <div className="max-w-[1240px] mx-auto flex flex-col px-4">
                 <div className='sm:flex justify-between items-center text-center'>
-                    <h1>Aceh.</h1>
+                    <h1>ACEH.</h1>
                     <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
                         <FaGithub className='icon' />
                         <FaInstagram className='icon' />

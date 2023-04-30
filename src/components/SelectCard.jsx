3219d33c545e2de75cdaaaaca46f01f76a77fb28
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-30 14:27:27 +0700

    adding img card

---
+++ src/components/SelectCard.jsx
@@ -0,0 +1,12 @@
+function SelectCard({ bg, text }) {
+    return <>
+        <div className='relative'>
+            <img className="w-full h-full object-cover" src={bg} alt="/" />
+            <div className=' absolute top-0 left-0 w-full h-full bg-gray-900/30'>
+                <p className='absolute bottom-4 left-4 text-2xl text-white font-bold'>{text}</p>
+            </div>
+        </div>
+    </>
+}
+
+export default SelectCard
